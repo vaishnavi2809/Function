894756097f9636dbfe458883277d4673f3a540ab
vaishnavi2809 <106094504+vaishnavi2809@users.noreply.github.com>
2022-05-30 22:56:40 +0530

    Add files via upload

---
+++ maths.py
@@ -0,0 +1,6 @@
+# Import the math module in whatever way you prefer. Call its sqrt function on the number 13689 and print that value to the console.
+
+import math as m
+n=int(input())
+a=m.sqrt(n)
+print(round(a,2))
